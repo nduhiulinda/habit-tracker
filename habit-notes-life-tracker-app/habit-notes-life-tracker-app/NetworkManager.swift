559c35d11c2885b940668d4560f26dae6c86311b
ryleehickman <hickryman@gmail.com>
2020-12-06 15:37:14 -0500

    Added .gitignore

---
+++ habit-notes-life-tracker-app/habit-notes-life-tracker-app/NetworkManager.swift
@@ -0,0 +1,9 @@
+//
+//  NetworkManager.swift
+//  habit-notes-life-tracker-app
+//
+//  Created by Rylee Hickman on 12/6/20.
+//  Copyright © 2020 Rylee Hickman. All rights reserved.
+//
+
+import Foundation
